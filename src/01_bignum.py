cfdfbd4b76acfa487609935a201542ebcb88720f
George Adams <georgeadams@DRs-MacBook-Pro.local>
2019-09-30 14:49:04 -0500

    completed 00 and 01

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,11 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
+#in JS, the output shows infinity. SMH. Python is sweet
+
 # YOUR CODE HERE
+
+x = 2**65536
+
+print(x)
+
