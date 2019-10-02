6d2e4e3277715ee590518c05b740c4e3c62af08e
George Adams <georgeadams@DRs-MacBook-Pro.local>
2019-10-02 15:13:54 -0500

    everything except 14

--- src/15_classes.py
+++ src/15_classes.py
@@ -61,5 +61,6 @@ geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 
 
-# Print it--also make this print more nicely
+# Print it--also make this print more nicely!
 print(geocache)
+
